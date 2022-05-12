fede0217ed768df105de401f01eb3955a2bffb8b
davidcoats <david@getsafetycone.com>
2022-05-12 15:20:48 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0137/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0137
+{
+    /// <summary>
+    /// Context marker attribute and interface types.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
