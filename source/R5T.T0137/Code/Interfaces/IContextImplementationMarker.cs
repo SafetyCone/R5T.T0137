262959a2ed86c77791327f87934fb7053ed21e07
davidcoats <david@getsafetycone.com>
2024-03-07 12:36:36 -0800

    Interim changes.

--- source/R5T.T0137/Code/Interfaces/IContextImplementationMarker.cs
+++ source/R5T.T0137/Code/Interfaces/IContextImplementationMarker.cs
@@ -4,7 +4,8 @@ using System;
 namespace R5T.T0137
 {
     /// <summary>
-    /// Marker interface indicate that a class is a context implementation.
+    /// Marker interface indicating that a class is a context implementation.
+    /// Allows methods-for, and extension methods-on, all types that are context implementations.
     /// </summary>
     public interface IContextImplementationMarker
     {
